41e3ffd4fd98aad8d27fc77b9dd990d6eff21dc2
2110103004 <joaomerlugo.aluno@unipampa.edu.br>
2022-08-06 23:33:51 -0300

    commit 0.0

---
+++ src/main/java/com/project/notsteam/NotsteamApplication.java
@@ -0,0 +1,13 @@
+package com.project.notsteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotsteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotsteamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/notsteam/NotsteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.notsteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotsteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
